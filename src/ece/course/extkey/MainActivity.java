5edef2c6ae70b2de04d333056d483329967d4bee
rcywongaa <rcywongaa@gmail.com>
2014-04-17 20:17:02 +0800

    Code move
    Originally contained some codes that I added for tab, code moved to
    ExtKeyTab
    (should not have any other changes)

--- src/ece/course/extkey/MainActivity.java
+++ src/ece/course/extkey/MainActivity.java
@@ -2,21 +2,13 @@ package ece.course.extkey;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.bluetooth.*;
 
 public class MainActivity extends Activity {
-
-	@Override
+		BluetoothAdapter mAdapter;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
