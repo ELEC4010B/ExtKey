4b41acb540651116f42b6689a17f9c541ad2e406
jj1118 <jj1118@hotmail.com>
2014-05-02 19:41:42 +0800

    Added more keys to numpad layout

--- gen/ece/course/extkey/R.java
+++ gen/ece/course/extkey/R.java
@@ -24,18 +24,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int btn0=0x7f08000b;
-        public static final int btn1=0x7f080008;
-        public static final int btn2=0x7f080009;
-        public static final int btn3=0x7f08000a;
-        public static final int btn4=0x7f080005;
-        public static final int btn5=0x7f080006;
-        public static final int btn6=0x7f080007;
-        public static final int btn7=0x7f080002;
-        public static final int btn8=0x7f080003;
-        public static final int btn9=0x7f080004;
+        public static final int action_settings=0x7f08001a;
+        public static final int btn0=0x7f080016;
+        public static final int btn1=0x7f080012;
+        public static final int btn2=0x7f080013;
+        public static final int btn3=0x7f080014;
+        public static final int btn4=0x7f08000e;
+        public static final int btn5=0x7f08000f;
+        public static final int btn6=0x7f080010;
+        public static final int btn7=0x7f08000a;
+        public static final int btn8=0x7f08000b;
+        public static final int btn9=0x7f08000c;
+        public static final int btnAnd=0x7f080003;
+        public static final int btnApostrophe=0x7f080015;
+        public static final int btnAt=0x7f080002;
+        public static final int btnBracketLeft=0x7f080004;
+        public static final int btnBracketRight=0x7f080005;
+        public static final int btnColon=0x7f080007;
+        public static final int btnComma=0x7f080017;
         public static final int btnConnect=0x7f080001;
+        public static final int btnDollar=0x7f080006;
+        public static final int btnDot=0x7f080018;
+        public static final int btnDoubleQuotation=0x7f080011;
+        public static final int btnExclamation=0x7f08000d;
+        public static final int btnHyphen=0x7f080008;
+        public static final int btnQuestion=0x7f080009;
+        public static final int btnSemicolon=0x7f080019;
         public static final int listview=0x7f080000;
     }
     public static final class layout {
