6cbf186f0e5f41bca665f85703c35fbff69174db
rcywongaa <rcywongaa@gmail.com>
2014-05-07 12:54:24 +0800

    trackpad2 contains multitouch code

---
+++ src/ece/course/extkey/Trackpad2.java
@@ -0,0 +1,131 @@
+package ece.course.extkey;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+
+public class Trackpad2 extends SurfaceView{
+	final int X = 0;
+	final int Y = 1;
+	final float X_THRESH = 30;
+	final float Y_THRESH = 80;
+	OutputStream mOutputStream;
+	String mMessage;
+	float tmpX;
+	float tmpY;
+	float prevX;
+	float prevY;
+	float dX;
+	float dY;
+	Paint paint;
+	
+	
+	public Trackpad2(Context context) {
+		super(context);
+		dX = 0;
+		dY = 0;
+		mOutputStream = ConnectActivity.mOutputStream;
+		paint = new Paint();
+		setWillNotDraw(false);
+	}
+	
+	public void onDraw(Canvas canvas){
+		if (canvas == null)
+			return;
+		canvas.drawColor(Color.LTGRAY);
+		canvas.drawPaint(paint);
+		paint.setColor(Color.GRAY);
+		paint.setTextSize(48);
+		paint.setTextAlign(Align.CENTER);
+		canvas.drawText("TrackPad", canvas.getWidth()*1/2, canvas.getHeight()*1/2, paint);
+	}
+	
+	public boolean onTouchEvent(MotionEvent motionEvent){
+		if (motionEvent.getPointerCount() == 1){
+			int action = motionEvent.getAction();
+			switch (action){
+			case MotionEvent.ACTION_DOWN : 
+				prevX = motionEvent.getX();
+				prevY = motionEvent.getY();
+	//			Log.i("Touch screen", "Pressed @X: " + prevX + ", Y: " + prevY);
+				break;
+				
+			case MotionEvent.ACTION_MOVE :
+				tmpX = motionEvent.getX();
+				tmpY = motionEvent.getY();
+				dX = dX + (tmpX - prevX);
+				dY = dY + (tmpY - prevY);
+				prevX = tmpX;
+				prevY = tmpY;
+				action = motionEvent.getAction();
+	//			Log.i("Touch screen", "Moved @X: " + tmpX + ", Y: " + tmpY);
+				
+				if (dX >= X_THRESH){
+					dX = 0;
+					mMessage = "R";
+					try {
+						mOutputStream.write(mMessage.getBytes());
+						Log.i("Sent ", mMessage);
+					} catch (IOException e) {
+						Log.i("Failed sending ", mMessage);
+						// e.printStackTrace();
+					}
+				}
+				else if (dX <= -X_THRESH){
+					dX = 0;
+					mMessage = "L";
+					try {
+						mOutputStream.write(mMessage.getBytes());
+						Log.i("Sent ", mMessage);
+					} catch (IOException e) {
+						Log.i("Failed sending ", mMessage);
+						// e.printStackTrace();
+					}
+				}
+				if (dY >= Y_THRESH){
+					dY = 0;
+					mMessage = "D";
+					try {
+						mOutputStream.write(mMessage.getBytes());
+						Log.i("Sent ", mMessage);
+					} catch (IOException e) {
+						Log.i("Failed sending ", mMessage);
+						// e.printStackTrace();
+					}
+				}
+				if (dY <= -Y_THRESH){
+					dY = 0;
+					mMessage = "U";
+					try {
+						mOutputStream.write(mMessage.getBytes());
+						Log.i("Sent ", mMessage);
+					} catch (IOException e) {
+						Log.i("Failed sending ", mMessage);
+						// e.printStackTrace();
+					}
+				}
+				
+				break;
+				
+			case MotionEvent.ACTION_UP : 
+	//			Log.i("Touch screen", "Released @dX: " + dX + ", dY: " + dY);
+				break;
+			}
+			return true;
+		}
+		else {
+			int index = motionEvent.findPointerIndex(motionEvent.getPointerId(1));
+			int action2 = motionEvent.getAction();
+			return true;
+		}
+	}
+}
